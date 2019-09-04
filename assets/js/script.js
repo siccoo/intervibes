5b475acf4304df89bb4837f33a38e30c84d927e8
Michael Chilaka <chilakamichael001@gmail.com>
2019-09-04 19:23:08 +0100

    image modal pop-up in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-// $('#myModal').on('shown.bs.modal', function () {
-//     $('#myModal').focus()
-//   })
+// // $('#myModal').on('shown.bs.modal', function () {
+// //     $('#myModal').focus()
+// //   })
 
 
-// Declaring an on click function with <a> class
-$(document).on("click", ".openImage", function() {
-  // Then get data from data-id
-  var myModal = $(this).data('assets/images/bcg3.jpg');
-  // After this passdata to img src on body
-  $(".modal-body, #myModal").attr("assets/images/bcg3.jpg", myModal);
+// // Declaring an on click function with <a> class
+// $(document).on("click", ".openImage", function() {
+//   // Then get data from data-id
+//   var myModal = $(this).data('assets/images/bcg3.jpg');
+//   // After this passdata to img src on body
+//   $(".modal-body, #myModal").attr("assets/images/bcg3.jpg", myModal);
+// });
+
+$(window).load(function() {
+  $('img').on('click', function() {
+    var src = $(this).attr('src');
+    $('#model-img').attr('src', src);
+    $('#imgId').modal('show');
+  });
 });
-
