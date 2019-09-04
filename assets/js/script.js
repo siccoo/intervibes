549bcdfba7b9deb16b0dddddd30716768bc3f216
Michael Chilaka <chilakamichael001@gmail.com>
2019-09-04 10:50:45 +0100

    Added functionality to images on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myModal').focus()
-  })
+// $('#myModal').on('shown.bs.modal', function () {
+//     $('#myModal').focus()
+//   })
+
+
+// Declaring an on click function with <a> class
+$(document).on("click", ".openImage", function() {
+  // Then get data from data-id
+  var myModal = $(this).data('assets/images/bcg3.jpg');
+  // After this passdata to img src on body
+  $(".modal-body, #myModal").attr("assets/images/bcg3.jpg", myModal);
+});
+
